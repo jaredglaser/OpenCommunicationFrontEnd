3bedbb669a30a86d206ce4669cc9ed3ef8baf5ed
Andrew Brenner <Abrenner@Andrews-MacBook-Pro.local>
2020-05-10 03:06:54 -0400

    ajax register post created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Test';
+  
 
   //TODO Determine whether or not user is logged in
   get loggedIn(): boolean {
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import * as $ from 'jquery';
+
+
 
 @Component({
   selector: 'app-register',
@@ -6,11 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  submitted: boolean = false;
 
 
   constructor() { }
 
   ngOnInit(): void {
   }
+ 
+  register(){
+    var user = (<HTMLInputElement>document.getElementById("username")).value;
+    var pwd = (<HTMLInputElement>document.getElementById("pwd")).value;
+    document.getElementById("heading").innerHTML = user;
+    
+    $.ajax({
+      type: "post",
+      url: "localhost:3000/api/security/register",
+      data: {
+        username: user,
+        password: pwd
+      },
+      success: function(respone){
+        console.log(respone);
+      }
+    });
+    console.log("ajax register post sent");
+  }
+
+
+
 
 }
