bbbd67322a58bde6c4ca304810adbb03c08f514b
Jared Glaser <Jared@glaserfamily.net>
2020-05-13 11:14:30 -0400

    webrtc basic communications

--- src/app/pages/ui/ui.component.ts
+++ src/app/pages/ui/ui.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Globals } from 'src/app/Globals';
+import { HttpClient } from '@angular/common/http';
+import * as SimplePeer from 'src/assets/simplepeer.min.js'
 
 class server{
   id : number;
@@ -26,15 +28,92 @@ const SERVERS: server[] = [
 })
 export class UIComponent implements OnInit {
   servers = SERVERS;
-  activeusers = [{id:1,name: "testuser"}];
+  peerid = "";
+  peer;
+  activeusers = [];
   username = localStorage.getItem('username');
-  constructor() { }
 
+  constructor(private http: HttpClient) { }
+
+ 
 
   ngOnInit(): void {
     
   }
-
+  createcall(){
+    this.peer = new SimplePeer({ initiator: true, trickle: false});
+    console.log("peer made");
+    this.peer.on('signal', data => {
+      console.log("signaled");
+      this.peerid = JSON.stringify(data);
+      console.log(JSON.stringify(data));
+    });
+    this.peer.on('error', err => console.log('error', err));
+    this.peer.on('stream', stream => {
+      console.log("got stream");
+      // got remote video stream, now let's show it in a video tag
+      var video = document.querySelector('video');
+      video.srcObject = stream;
+      video.play();
+    });
+    
+  }
+  joincallHost(peerstring){
+    this.peer.signal(JSON.parse(peerstring));
+    this.peer.on('connect', () => {
+      console.log('CONNECTED');
+      this.peer.send("message from the host");
+    });
+    this.peer.on('data', data => {
+      console.log('data: ' + data)
+    })
+  }
+  joincall(peerstring){
+    //if(this.peer != null){ //host is joining
+    //  this.peer.signal(JSON.parse(peerstring));
+    //}
+    //else{
+    this.peer = new SimplePeer({ initiator : false, trickle: false});
+    console.log("peer made");
+    this.peer.on('signal', data => {
+      console.log("peer data recieved");
+      this.peerid = JSON.stringify(data);
+      console.log(JSON.stringify(data));
+    });
+    this.peer.on('error', err => console.log('error', err));
+    console.log("peer signaled");
+    this.peer.signal(JSON.parse(peerstring));
+    this.peer.on('stream', stream => {
+      console.log("got remote stream");
+      // got remote video stream, now let's show it in a video tag
+      var video = document.querySelector('video');
+      video.srcObject = stream;
+      video.play();
+    });
+    this.peer.on('connect', () => {
+      console.log('CONNECTED');
+      this.peer.send("message from the guest");
+    });
+    this.peer.on('data', data => {
+      console.log('data: ' + data)
+    })
+  //}
+  }
+  shareVideo(){
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    }).then(this.gotMedia).catch(() => {});
+  }
+  gotMedia (stream) {
+    console.log("adding stream");
+    this.peer.addStream(stream);  
+  }
+  refreshUsers(){
+    this.http.get<any>(Globals.ip+":"+Globals.port+"/api/").subscribe( response => {
+      console.log("test");
+    });
+  }
   
 
 }
