ebd4ab960432dee7a2517ac347f17904fc0b83ee
Jared Glaser <Jared@glaserfamily.net>
2020-05-16 18:02:37 -0400

    fixed highlighting

--- src/app/pages/ui/ui.component.ts
+++ src/app/pages/ui/ui.component.ts
@@ -3,52 +3,52 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 
-class server{
-  id : number;
-  name : string;
+class server {
+  id: number;
+  name: string;
 }
 
-class room{
-  id : number;
-  name : string;
+class room {
+  id: number;
+  name: string;
   server: number;
 }
 
-class user{
-  id : number;
+class user {
+  id: number;
   username: string;
 }
 
-class message{
+class message {
   time: string;
   from: string;
   classcontainer: string;
   classimage: string;
   timeclass: string;
-  usernameclass:string;
+  usernameclass: string;
   content: string;
 }
 
 const ROOMS: room[] = [
   { id: 101, name: "myroom", server: 11 },
-  { id: 102, name: "aroom", server: 11},
-  { id: 103, name: "anotherroom", server: 12},
-  { id: 104, name: "room4", server: 12},
-  { id: 105, name: "room5", server: 13},
-  { id: 106, name: "room6", server: 13},
-  { id: 107, name: "room7", server: 14},  
-  { id: 108, name: "room8", server: 15},
+  { id: 102, name: "aroom", server: 11 },
+  { id: 103, name: "anotherroom", server: 12 },
+  { id: 104, name: "room4", server: 12 },
+  { id: 105, name: "room5", server: 13 },
+  { id: 106, name: "room6", server: 13 },
+  { id: 107, name: "room7", server: 14 },
+  { id: 108, name: "room8", server: 15 },
 ];
 
 const MESSAGES: message[] = [
-  { time: "11:00", from: "otheruser", classcontainer:"container", classimage: "", timeclass:"time-right" , usernameclass:"username-left", content:"whats up"},
-  { time: "11:00", from: "jaredtest", classcontainer:"container darker",classimage: "right",timeclass:"time-left" , usernameclass:"username-right",content:"whats up"},
-  { time: "11:00", from: "otheruser", classcontainer:"container",classimage: "",timeclass:"time-right" , usernameclass:"username-left",content:"whats up"},
-  { time: "11:00", from: "otheruser", classcontainer:"container",classimage: "",timeclass:"time-right" , usernameclass:"username-left",content:"whats up"},
-  { time: "11:00", from: "jaredtest", classcontainer:"container darker",classimage: "right",timeclass:"time-left" , usernameclass:"username-right",content:"whats up"},
-  { time: "11:00", from: "jaredtest", classcontainer:"container darker",classimage: "right",timeclass:"time-left" , usernameclass:"username-right",content:"whats up"},
-  { time: "11:00", from: "otheruser", classcontainer:"container",classimage: "",timeclass:"time-right" , usernameclass:"username-left",content:"whats up"},
-  { time: "11:00", from: "jaredtest", classcontainer:"container darker",classimage: "right",timeclass:"time-left" , usernameclass:"username-right",content:"whats up"},
+  { time: "11:00", from: "otheruser", classcontainer: "container", classimage: "", timeclass: "time-right", usernameclass: "username-left", content: "whats up" },
+  { time: "11:00", from: "jaredtest", classcontainer: "container darker", classimage: "right", timeclass: "time-left", usernameclass: "username-right", content: "whats up" },
+  { time: "11:00", from: "otheruser", classcontainer: "container", classimage: "", timeclass: "time-right", usernameclass: "username-left", content: "whats up" },
+  { time: "11:00", from: "otheruser", classcontainer: "container", classimage: "", timeclass: "time-right", usernameclass: "username-left", content: "whats up" },
+  { time: "11:00", from: "jaredtest", classcontainer: "container darker", classimage: "right", timeclass: "time-left", usernameclass: "username-right", content: "whats up" },
+  { time: "11:00", from: "jaredtest", classcontainer: "container darker", classimage: "right", timeclass: "time-left", usernameclass: "username-right", content: "whats up" },
+  { time: "11:00", from: "otheruser", classcontainer: "container", classimage: "", timeclass: "time-right", usernameclass: "username-left", content: "whats up" },
+  { time: "11:00", from: "jaredtest", classcontainer: "container darker", classimage: "right", timeclass: "time-left", usernameclass: "username-right", content: "whats up" },
 ];
 
 
@@ -61,9 +61,9 @@ const SERVERS: server[] = [
 ];
 
 const FRIENDS: user[] = [
-  { id: 1001, username: 'bestFriend'},
-  { id: 1002, username: 'myFriend'},
-  { id: 1003, username: 'aFriend'},
+  { id: 1001, username: 'bestFriend' },
+  { id: 1002, username: 'myFriend' },
+  { id: 1003, username: 'aFriend' },
 ];
 
 
@@ -79,19 +79,59 @@ export class UIComponent implements OnInit {
   messages = MESSAGES;
   rooms = ROOMS;
   friends = FRIENDS;
-  activeusers = [{id:1,name: "testuser"}];
+  activeusers = [{ id: 1, name: "testuser" }];
   username = localStorage.getItem('username');
-  
+
 
   constructor() { }
 
 
   ngOnInit(): void {
     document.getElementById("refresh").addEventListener("click", this.refreshFriendsList, false);
+
+
+
+  }
+  buttonclicked(button: HTMLButtonElement): void {
+
+  }
+
+  ngAfterViewInit() {
+    var def = this;
+    Array.from(document.getElementsByClassName("list-group-item-action")).forEach(element => {
+      element.addEventListener("click", function () {
+        console.log(element.parentNode);
+        console.log(element.parentNode.previousSibling.textContent);
+        if (element.parentNode.previousSibling.textContent === "Servers") {
+          Array.from(document.getElementsByClassName("list-group-item-action")).forEach(btn => {
+            console.log("removed class");
+            btn.classList.remove("selected-list-item");
+          })
+          element.classList.add("selected-list-item");
+        }
+        else if (element.parentNode.previousSibling.textContent.trim() === "Friends") {
+          Array.from(document.getElementsByClassName("list-group-item-action")).forEach(btn => {
+            console.log("removed class");
+            if (btn.parentNode.previousSibling.textContent !== "Servers") {
+              btn.classList.remove("selected-list-item");
+            }
+            element.classList.add("selected-list-item");
+          });
+      }
+        else if (element.parentNode.previousSibling.textContent === "Rooms") {
+          Array.from(document.getElementsByClassName("list-group-item-action")).forEach(btn => {
+            if (btn.parentNode.previousSibling.textContent !== "Servers") {
+              btn.classList.remove("selected-list-item");
+            }
+          })
+          element.classList.add("selected-list-item");
+        }
+      });
+    });
   }
 
   //TODO GET request to get current friends list
-  refreshFriendsList(): void{
+  refreshFriendsList(): void {
     console.log("refresh");
   }
 
